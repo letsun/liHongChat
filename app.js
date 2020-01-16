8b91a91dc08b862deaf7c3db3e5508cce181b6b2
huyao95 <huyao95@example.com>
2020-01-16 17:59:27 +0800

    20200116

--- app.js
+++ app.js
@@ -2,35 +2,16 @@ const RdWXBizDataCrypt = require('utils/WXBizDataCrypt.js');
 App({
 
   globalData: {
-<<<<<<< HEAD
     memberId: '1',
     //memberId: '',
     idData: {
       // openid: '',
       openid: 'ofjyq5bIZoFepEV22XbqTsxAtkI0',
-=======
-    memberId: '2',
-    idData: {
-      openid: 'ofjyq5X3IvRpUGmOtXY0CMCE5odI',
->>>>>>> bd9ccd18970b2b6642b0bd39d93a38827bcc55a4
       apipwd:'',
       token:'',
       sessionKey:'',
       unionId: "",
     },
-<<<<<<< HEAD
-
-=======
-    addressComponent: {
-      province: '',
-      city: '',
-      district: '',
-      street: '',
-      town: '',
-    },
-    baseURL: 'http://192.168.1.105:18001/',
-    baseURL2: 'http://192.168.1.105:8085/',
->>>>>>> bd9ccd18970b2b6642b0bd39d93a38827bcc55a4
     goodsId:'',//商品id
     companyId:'' //企业id
   }
