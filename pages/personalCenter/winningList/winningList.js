e2f829550af47bcb4f0b15fdfe69b9b1550ce658
huyao95 <huyao95@example.com>
2020-06-10 14:47:49 +0800

    6.10

--- pages/personalCenter/winningList/winningList.js
+++ pages/personalCenter/winningList/winningList.js
@@ -6,75 +6,86 @@ const api = require("../../../utils/api.js")
 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        imapath:app.globalData.imapath,
-        pageNum: 1,
-        lotteryList: '',
-        loadingtext:'上拉加载更多'
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imapath: app.globalData.imapath,
+    pageNum: 1,
+    lotteryList: '',
+    loadingtext: '上拉加载更多'
+  },
 
-    onShow(){
-        let that =this;
-        that.lotteryList()
-    },
+  onReady() {
+    let that = this;
+    that.lotteryList()
+  },
+
+  //上拉加载
+  pullup() {
+    let that = this;
+    that.setData({
+      pageNum: that.data.pageNum + 1
+    })
+
+    if (that.data.hasNext == "true") {
+      that.lotteryList()
+    } else {
+      that.setData({
+        loadingtext: '没有更多数据了'
+      })
+
+    }
+
+  },
+
+  lotteryList() {
+    let that = this;
+    let pageNum = that.data.pageNum;
+    common.requestPosts(api.lotteryList + app.globalData.memberId, {
+      pageNum: pageNum,
+    }, res => {
+      let lotteryList = that.data.lotteryList;
+      let hasNext = that.data.hasNext;
+
+      let lotteryLista = res.data.data.lotteryList;
+
+      for (var i in lotteryLista) {
+        lotteryLista[i].lotteryTime = lotteryLista[i].lotteryTime.split(' ')[0]
+      }
 
 
-    pullup() {
-        let that = this;
+      if (lotteryList == '') {
+
         that.setData({
-          pageNum: that.data.pageNum + 1
+          lotteryList: lotteryLista,
+          hasNext: res.data.data.hasNext
         })
 
-        if (that.data.hasNext =="true") {
-            that.lotteryList()
-        }else {
-            that.setData({
-                loadingtext:'没有更多数据了'
-            })
-            
+        if (res.data.data.hasNext == "false") {
+          that.setData({
+            loadingtext: '没有更多数据了'
+          })
         }
+      } else {
+        if (hasNext == "true") {
+          that.setData({
+            lotteryList: lotteryList.concat(lotteryLista),
+            hasNext: res.data.data.hasNext
+          })
+        }
+      }
+    })
+  },
 
-    },
-
-    lotteryList() {
-        let that = this;
-        let pageNum = that.data.pageNum;
-        common.requestPosts(api.lotteryList + app.globalData.memberId, {
-          pageNum: pageNum,
-        }, res => {
-          let lotteryList = that.data.lotteryList;
-          let hasNext = that.data.hasNext;
-
-          let lotteryLista = res.data.data.lotteryList;
-            
-          for (var  i in lotteryLista) {      
-              lotteryLista[i].lotteryTime =  lotteryLista[i].lotteryTime.split(' ')[0]
-          }
-
-
-          if (lotteryList == '') {
-            
-            that.setData({
-              lotteryList: lotteryLista,
-              hasNext: res.data.data.hasNext
-            })
-
-            if (res.data.data.hasNext =="false") {
-              that.setData({
-                loadingtext:'没有更多数据了'
-            })
-            }
-          } else {
-            if (hasNext == "true") {
-              that.setData({
-                lotteryList: lotteryList.concat(lotteryLista),
-                hasNext: res.data.data.hasNext
-              })
-            }
-          }
-        })
-      },
+  //跳转到中奖记录
+  winning() {
+    if (app.globalData.memberId > 0) {
+      wx.navigateTo({
+        url: '../../personalCenter/winning/winning',
+      })
+    } else {
+      common.login()
+    }
+  },
 })
